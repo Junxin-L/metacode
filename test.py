88a59f2ec4a85b3ca50138d59e7f2a5f5af0df20
Camille Leisen <liujunxin0929@gmail.com>
2023-03-18 16:38:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from math import sqrt
-import shuffle2 as s
+import shuffle as s
 
 i = ['123', 'aaa', 'bbb', 'usdhus', '23213', "22", "234", 123, 14342, 21, 677, 564, 56456, 5463]
 i = [i*8 for i in range(20)]
